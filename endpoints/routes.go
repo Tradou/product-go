98bb4f80cbffb030564c33f35ba225f8792b36d2
UrTeddy on drugs <trad.aidoud@gmail.com>
2023-06-06 15:02:55 +0200

    refactor(route): remove useless route

--- endpoints/routes.go
+++ endpoints/routes.go
@@ -5,12 +5,6 @@ import "github.com/gin-gonic/gin"
 func RegisterEndpoints() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello",
-		})
-	})
-
 	RegisterProductEndpoint(router)
 	router.Run(":80")
 }
