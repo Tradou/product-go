00178808fe752ccb45ae022ecb29742506256282
UrTeddy on drugs <trad.aidoud@gmail.com>
2023-06-07 15:37:53 +0200

    feat(products): add stock

--- infrastructure/models/products.go
+++ infrastructure/models/products.go
@@ -9,6 +9,7 @@ type Product struct {
 	Name        string
 	Description string
 	Price       uint
+	Stock       uint
 	State       bool
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
@@ -18,6 +19,7 @@ type StoreProduct struct {
 	Name        string `json:"name" binding:"required"`
 	Description string `json:"description" binding:"required"`
 	Price       uint   `json:"price" binding:"required"`
+	Stock       uint   `json:"stock" binding:"required"`
 	State       *bool  `json:"state" binding:"required"`
 }
 
@@ -25,6 +27,7 @@ type UpdateProduct struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Price       uint   `json:"price"`
+	Stock       uint   `json:"stock"`
 	State       *bool  `json:"state"`
 }
 
