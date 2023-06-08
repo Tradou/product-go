5cfdd87e9fc6f35260da2bcd167fb0ce983f8d5e
UrTeddy on drugs <trad.aidoud@gmail.com>
2023-06-08 19:52:25 +0200

    feat(products): add relationship

--- handlers/product.go
+++ handlers/product.go
@@ -17,7 +17,7 @@ func GetProducts(c *gin.Context) {
 		return
 	}
 
-	if err := db.Find(&products).Error; err != nil {
+	if err := db.Preload("Attributes.AttributeProducts").Find(&products).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
--- infrastructure/models/products.go
+++ infrastructure/models/products.go
@@ -7,13 +7,14 @@ import (
 
 type Product struct {
 	ID          uint
-	Name        string    `gorm:"not null;size:64"`
-	Description string    `gorm:"not null"`
-	Price       uint      `gorm:"not null"`
-	Stock       uint      `gorm:"not null"`
-	State       bool      `gorm:"not null"`
-	CreatedAt   time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
-	UpdatedAt   time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
+	Name        string      `gorm:"not null;size:64"`
+	Description string      `gorm:"not null"`
+	Price       uint        `gorm:"not null"`
+	Stock       uint        `gorm:"not null"`
+	State       bool        `gorm:"not null"`
+	CreatedAt   time.Time   `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
+	UpdatedAt   time.Time   `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
+	Attributes  []Attribute `gorm:"many2many:attribute_products;"`
 }
 
 type StoreProduct struct {
