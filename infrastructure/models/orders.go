b74dd6506822a879f7ea5eccf284549925f21e5f
UrTeddy on drugs <trad.aidoud@gmail.com>
2023-06-09 11:47:40 +0200

    fix(orders): rename column amount

--- infrastructure/models/orders.go
+++ infrastructure/models/orders.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Order struct {
-	ID          uint
-	TotalAmount uint        `gorm:"not null"`
-	OrderItems  []OrderItem `gorm:"foreignKey:OrderID"`
-	CreatedAt   time.Time   `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
-	UpdatedAt   time.Time   `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
+	ID         uint
+	Amount     uint        `gorm:"not null"`
+	OrderItems []OrderItem `gorm:"foreignKey:OrderID"`
+	CreatedAt  time.Time   `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
+	UpdatedAt  time.Time   `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
 }
