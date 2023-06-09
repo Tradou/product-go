73fed0ee132cddc59a36301f6b8a363611bebb06
UrTeddy on drugs <trad.aidoud@gmail.com>
2023-06-09 11:46:03 +0200

    fix(orders): remove orders.reference column

--- infrastructure/models/orders.go
+++ infrastructure/models/orders.go
@@ -4,7 +4,6 @@ import "time"
 
 type Order struct {
 	ID          uint
-	Reference   string      `gorm:"unique;size:8"`
 	TotalAmount uint        `gorm:"not null"`
 	OrderItems  []OrderItem `gorm:"foreignKey:OrderID"`
 	CreatedAt   time.Time   `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
