96403af59fbfe301f642341f5e34fe3e65552424
UrTeddy on drugs <trad.aidoud@gmail.com>
2023-06-09 11:27:44 +0200

    feat(orders): add orders endpoints

---
+++ endpoints/orders.go
@@ -0,0 +1,16 @@
+package endpoints
+
+import (
+	"github.com/gin-gonic/gin"
+	"main/handlers"
+)
+
+func RegisterOrderEndpoint(router *gin.Engine) {
+	orders := router.Group("/orders")
+	{
+		orders.GET("", handlers.GetOrders)
+		orders.GET("/:id", handlers.GetOrder)
+		orders.POST("", handlers.StoreOrder)
+		orders.PATCH("/:id", handlers.UpdateOrder)
+	}
+}
